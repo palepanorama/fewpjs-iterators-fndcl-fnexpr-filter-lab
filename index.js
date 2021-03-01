9f51768de211f05ea16474e4a300c2c48bf05bea
palepanorama <matthew.keith.thomas@gmail.com>
2021-03-01 17:30:21 -0600

    commit tree cb446fca0e4c93464cd44919163e52f552f21135

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string) {
+    let drivers = array.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+    return drivers;
+}
+
+function fuzzyMatch(array, letters) {
+    let drivers = array.filter(driver => {
+        return driver.toLowerCase().startsWith(letters.toLowerCase())
+    })
+    return drivers;
+}
+
+function matchName(array, string) {
+    let drivers = array.filter(driver => {
+        return driver.name.toString().toLowerCase() === string.toLowerCase()
+    })
+    return drivers;
+}
